2f92a1e21459f3394f5a8c5ea4681f672c4f8010
mcaponera <m.caponera@gmail.com>
2023-05-15 14:12:11 -0300

    adicionando consultas

--- queries.sql
+++ queries.sql
@@ -1,12 +1,14 @@
+USE ecommerce;
+
 -- juntadando vendedores e produtos
 SELECT p.nome_produto, pj.razaoSocial, pj.enderecoID, p.produtoID, p.tamanho
 	FROM produto p INNER JOIN cliente_pj pj
-	ON p.vendedorID = pj.vendedorID;
+		ON p.vendedorID = pj.vendedorID;
 
 -- juntando produto e estoque
 SELECT p.produtoID, p.nome_produto, e.quantidadeProduto
 	FROM produto p INNER JOIN estoque e
-	ON p.produtoID = e.produtoID;
+		ON p.produtoID = e.produtoID;
 
 
 -- listando todos os clientes, fornecedores e vendedores, seus emails, UFs e cidades    
@@ -17,3 +19,22 @@ LEFT JOIN (
     UNION ALL
     SELECT enderecoID, concat(nomeClientePF, " ", sobrenomeClientePF), cpfClientePF, emailClientePF FROM cliente_pf
 ) c ON e.enderecoID = c.enderecoID;
+
+-- agrupando produtos por vendedores/fornecedores
+SELECT cpj.razaoSocial, p.nome_produto, COUNT(*) AS total_produtos
+	FROM produto p
+JOIN cliente_pj cpj ON p.vendedorID = cpj.vendedorID OR p.fornecedorID = cpj.fornecedorID
+GROUP BY cpj.razaoSocial, p.nome_produto;
+
+-- agrupando vendedores e fornecedores que possuam mais de 2 produtos a venda
+SELECT cpj.razaoSocial, COUNT(*) AS total_produtos
+FROM produto p
+JOIN cliente_pj cpj ON p.vendedorID = cpj.vendedorID OR p.fornecedorID = cpj.fornecedorID
+GROUP BY cpj.razaoSocial
+HAVING COUNT(*) > 2;
+
+-- ordenando os produtos por seus valores
+SELECT p.produtoID, p.nome_produto, cpj.razaoSocial, p.tamanho, p.valor
+	FROM produto p
+JOIN cliente_pj cpj ON p.vendedorID = cpj.vendedorID OR p.fornecedorID = cpj.fornecedorID
+ORDER BY valor;
